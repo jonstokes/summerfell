a06b9a97a03270bfc1a651db34171e4c93df9707
Jon Stokes <jon@jonstokes.com>
2015-09-26 09:40:53 -0500

    Require address line 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :address_line_1, presence: true
 end
