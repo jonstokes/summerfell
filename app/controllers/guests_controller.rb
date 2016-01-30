191610b75661c0b36c3079f1b8b41aa217c1e973
Jon Stokes <jon@jonstokes.com>
2016-01-30 10:10:59 -0600

    Rip out unused guest controller methods

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,25 +1,8 @@
 class GuestsController < ApplicationController
-  before_action :set_guest, only: [:show, :edit, :update, :destroy]
-
-  # GET /guests
-  # GET /guests.json
-  def index
-    @guests = Guest.all
-  end
-
-  # GET /guests/1
-  # GET /guests/1.json
-  def show
-  end
-
   # GET /guests/new
   def new
     @packages = FindPackagesForGuest.call(device_address: params[:guest_mac]).packages
     @guest = Guest.new
-  end
-
-  # GET /guests/1/edit
-  def edit
   end
 
   # POST /guests
@@ -38,35 +21,7 @@ class GuestsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /guests/1
-  # PATCH/PUT /guests/1.json
-  def update
-    respond_to do |format|
-      if @guest.update(guest_params)
-        format.html { redirect_to @guest, notice: 'Guest was successfully updated.' }
-        format.json { render :show, status: :ok, location: @guest }
-      else
-        format.html { render :edit }
-        format.json { render json: @guest.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /guests/1
-  # DELETE /guests/1.json
-  def destroy
-    @guest.destroy
-    respond_to do |format|
-      format.html { redirect_to guests_url, notice: 'Guest was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_guest
-      @guest = Guest.find(params[:id])
-    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def guest_params
