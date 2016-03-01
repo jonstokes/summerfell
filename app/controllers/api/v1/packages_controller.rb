d766eb25dd7f6c2e3ca410fc02aad1e16d4292b3
Jon Stokes <jon@jonstokes.com>
2016-03-01 17:17:52 -0600

    Fixes

--- app/controllers/api/v1/packages_controller.rb
+++ app/controllers/api/v1/packages_controller.rb
@@ -1,10 +1,10 @@
 class API::V1::PackagesController < API::V1::BaseController
-  # GET api/packages/available/:device_address
+  # GET api/packages/available.json/?device_address=:addr
   def available
     packages = FindPackagesForGuest.call(device_address: params[:device_address]).packages
 
     respond_to do |format|
-      format.json { render json: packages, each_serializer: API::PackageSerializer }
+      format.json { render json: packages, each_serializer: API::V1::PackageSerializer }
     end
   end
 end
