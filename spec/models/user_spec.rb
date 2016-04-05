feadf3fe9f74876ec4ce6f8e9a44c6a62a4026c6
Jon Stokes <jon@jonstokes.com>
2016-04-04 20:43:44 -0500

    User specs are green

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    subject { build(:user) }
+
+    it "is valid" do
+      expect(subject).to be_valid
+    end
+
+    it "requires email" do
+      subject.email = nil
+      expect(subject).not_to be_valid
+    end
+
+    it "requires passwords for a new record" do
+      subject.password = nil
+      expect(subject).not_to be_valid
+    end
+
+    it "does not require a password for an existing record" do
+      subject.save
+      user = User.first
+      user.password = nil
+      expect(user).to be_valid
+    end
+  end
 end
